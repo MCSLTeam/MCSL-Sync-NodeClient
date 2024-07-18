0086c05e43d78ebf7631bb5b26a6cd74ad4c326e
Tigercrl <tigercrl@icloud.com>
2024-07-19 00:00:49 +0800

    fix: use ThreadPoolExecutor instead of threads

--- src/utils/update.py
+++ src/utils/update.py
@@ -1,7 +1,6 @@
 import os
 from asyncio import run
-from threading import Thread
-from time import sleep
+from concurrent.futures import ThreadPoolExecutor
 
 from .database import (
     available_downloads,
@@ -29,45 +28,27 @@ async def sync_database():
 
 
 class FileSync:
-    threadCount = 0
 
     def __init__(self, upd: list | str = "all"):
         self.update_core_list = available_downloads if upd == "all" else upd.split(",")
 
     def load_self(self):
-        threads = []
-        for core in self.update_core_list:
-            thread = Thread(target=self.load_single_core, args=(core,))
-            thread.start()
-            threads.append(thread)
-        for thread in threads:
-            thread.join()
-
-    def load_single_core(self, core_type: str):
-        mc_versions_list = get_mc_versions(
-            database_type="upstream", core_type=core_type
-        )
-        os.makedirs(f"files/{core_type}", exist_ok=True)
-        for mc_version in mc_versions_list:
-            self.load_single_version(core_type=core_type, mc_version=mc_version)
-
-    def load_single_version(self, core_type: str, mc_version: str):
-        core_versions_list = get_core_versions(
-            database_type="upstream", core_type=core_type, mc_version=mc_version
-        )
-        os.makedirs(f"files/{core_type}/{mc_version}", exist_ok=True)
-
-        def thread_creator():
-            for core_version in core_versions_list:
-                t = Thread(target=self.load_single_build, args=(core_type, mc_version, core_version))
-                while self.threadCount >= cfg.get("max_threads"):
-                    sleep(0.5)
-                t.start()
-                self.threadCount += 1
-
-        thread = Thread(target=thread_creator)
-        thread.start()
-        thread.join()
+        with ThreadPoolExecutor(max_workers=cfg.get("max_threads")) as executor:
+            futures = []
+            for core_type in self.update_core_list:
+                mc_versions_list = get_mc_versions(
+                    database_type="upstream", core_type=core_type
+                )
+                os.makedirs(f"files/{core_type}", exist_ok=True)
+                for mc_version in mc_versions_list:
+                    core_versions_list = get_core_versions(
+                        database_type="upstream", core_type=core_type, mc_version=mc_version
+                    )
+                    os.makedirs(f"files/{core_type}/{mc_version}", exist_ok=True)
+                    for core_version in core_versions_list:
+                        executor.submit(self.load_single_build, core_type, mc_version, core_version)
+            for future in futures:
+                future.result()
 
     def load_single_build(
             self, core_type: str, mc_version: str, core_version: str
